12a20546d322e3417fd47d776071a66725365ac9
vishakha Agalawe <aglawev4@gmail.com>
2024-09-05 07:24:20 -0500

    Resolved merge conflicts

--- Portfolio/assets/vendor/bootstrap/js/bootstrap.js
+++ Portfolio/assets/vendor/bootstrap/js/bootstrap.js
@@ -1,7 +1,4 @@
-/*!
-  * Bootstrap v5.3.3 (https://getbootstrap.com/)
-  * Copyright 2011-2024 The Bootstrap Authors (https://github.com/twbs/bootstrap/graphs/contributors)
-  * Licensed under MIT (https://github.com/twbs/bootstrap/blob/main/LICENSE)
+
   */
 (function (global, factory) {
   typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory(require('@popperjs/core')) :
@@ -76,12 +73,7 @@
     }
   };
 
-  /**
-   * --------------------------------------------------------------------------
-   * Bootstrap util/index.js
-   * Licensed under MIT (https://github.com/twbs/bootstrap/blob/main/LICENSE)
-   * --------------------------------------------------------------------------
-   */
+  
 
   const MAX_UID = 1000000;
   const MILLISECONDS_MULTIPLIER = 1000;
